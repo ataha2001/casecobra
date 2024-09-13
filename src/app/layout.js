fe4aaac6d5d11033e5446c5b5f6d7b7293e9af4e
ashraf taha <atahaegy2001@gmail.com>
2024-09-13 08:06:58 +0300

    first push to deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const recursive = Recursive({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={recursive.className}>
+      <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>
+          <Navbar />
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
